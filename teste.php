3115e2b39f19736801a27f1c13bdb27206a14a46
Saulo <saulocn@gmail.com>
2016-05-09 15:55:00 -0300

    Retirando parágrafos errados.

--- teste.php
+++ teste.php
@@ -4,5 +4,15 @@
   </head>
   <body>
     <h1>Projeto!</h1>
-  </body>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum diam nec arcu pulvinar congue. Mauris euismod lacus quis metus rutrum gravida. Quisque facilisis viverra leo in finibus. Pellentesque quis accumsan purus. Vivamus aliquam lectus pulvinar ligula pellentesque tristique. Nullam pretium dolor et sapien elementum, ut fermentum enim sollicitudin. Morbi ut lacus sit amet justo luctus suscipit. Praesent libero magna, pretium eget posuere eget, dignissim ut orci. In ultrices lobortis maximus. Curabitur neque nibh, vehicula quis suscipit non, consectetur eu nibh. Nullam eleifend euismod nulla, commodo porta lorem ultrices ut. Duis elementum faucibus rutrum. Pellentesque dapibus erat eros, vel maximus eros vehicula et. Etiam maximus viverra tellus, nec consectetur sem interdum vitae.
+
+    Etiam varius finibus lacus in sodales. Morbi nisi nisl, pulvinar vel elit commodo, ultricies semper quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris bibendum ac purus eu congue. Nulla semper mi nibh, at vestibulum nibh iaculis vestibulum. Nulla ut consectetur eros. Fusce lobortis sem lacus. Etiam efficitur molestie nibh, interdum faucibus neque congue in. Fusce sodales eros eu ipsum accumsan, ac tristique urna vehicula.
+
+    Aenean bibendum ipsum dictum, scelerisque eros ac, vehicula felis. Praesent lobortis justo et justo malesuada iaculis. Sed volutpat sit amet sapien vitae suscipit. Aenean nisi metus, malesuada at congue in, cursus sit amet lorem. Aliquam tincidunt malesuada turpis tempor accumsan. Sed gravida ligula enim, vitae ornare urna dignissim in. Quisque dignissim odio dui, quis aliquam augue tempus id. Pellentesque ultricies euismod dolor, maximus consectetur ipsum tincidunt sit amet. Nulla felis tortor, ullamcorper a sapien vitae, condimentum egestas neque. Aliquam tempor massa dui, nec accumsan eros placerat ac. Integer eget tincidunt mi, mattis elementum lacus. Duis lobortis nibh eu nunc eleifend facilisis. Aenean velit nisi, ultrices id maximus tempor, finibus in velit.
+
+    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse consequat pulvinar augue, ut lacinia nibh auctor vitae. Integer et metus quam. Nullam nisl elit, sodales eu velit nec, elementum tempor massa. Sed consectetur a enim ut semper. Fusce imperdiet tincidunt lorem non laoreet. Donec ultrices, mi non lobortis malesuada, augue sapien rutrum felis, vel malesuada enim nulla nec odio. Nam auctor nec nisl eget finibus. Nulla lorem lacus, porttitor non est at, convallis blandit tortor.
+
+    Integer pellentesque velit turpis, eget tempor nibh consequat nec. Donec vestibulum aliquam sollicitudin. Proin a nunc laoreet, ornare nisi at, sodales tortor. Nam vulputate egestas felis et pellentesque. Sed blandit maximus tempor. Maecenas nisi orci, malesuada id neque vitae, facilisis lobortis dui. Praesent sollicitudin dui sed eleifend aliquam. Suspendisse ac convallis mauris. Quisque et commodo ipsum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+      </body>
 </html>
